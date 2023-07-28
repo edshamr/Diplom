0745d0558d5dd3a9ee3c46e611eb9021482a2e74
Danbug <unwantedmouth@gmail.com>
2023-07-28 12:44:52 +0300

    Add db connectivity, add security defaults and welcome page

---
+++ src/main/java/com/productShop/inventarization/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.productShop.inventarization.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@AllArgsConstructor
+public class MainController {
+
+    @GetMapping("/")
+    public String helloWorld() {
+        return "index";
+    }
+}
