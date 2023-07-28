2318e7db8d813c700c76b16e83268212555f5725
Danbug <unwantedmouth@gmail.com>
2023-07-28 14:29:29 +0300

    Main page and table page creation

--- src/main/java/com/productShop/inventarization/controller/MainController.java
+++ src/main/java/com/productShop/inventarization/controller/MainController.java
@@ -1,16 +1,28 @@
 package com.productShop.inventarization.controller;
 
+import com.productShop.inventarization.service.ProductService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @AllArgsConstructor
 public class MainController {
+    ProductService productService;
 
     @GetMapping("/")
-    public String helloWorld() {
+    public String mainPage(Model model) {
+        var productsList = productService.getAllProducts();
+        model.addAttribute("products", productsList);
         return "index";
+    }
+
+    @GetMapping("/table")
+    public String tableViewPage(Model model) {
+        var productsList = productService.getAllProducts();
+        model.addAttribute("products", productsList);
+        return "table";
     }
 
     @GetMapping("/login")
