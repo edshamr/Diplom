f1a61c8236a407f992cd1d8dcf03a20d37470ab0
Danbug <unwantedmouth@gmail.com>
2023-07-28 12:20:02 +0300

    Initial commit

---
+++ src/main/java/com/productShop/inventarization/InventarizationApplication.java
@@ -0,0 +1,13 @@
+package com.productShop.inventarization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventarizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventarizationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/productShop/inventarization/InventarizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.productShop.inventarization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventarizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
