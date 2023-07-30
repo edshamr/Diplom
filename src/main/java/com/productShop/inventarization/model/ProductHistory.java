87990d35b90734bbd49ee4ca459a3769b2bfd3cc
Danbug <unwantedmouth@gmail.com>
2023-07-30 16:37:16 +0300

    Add many sells to one item

--- src/main/java/com/productShop/inventarization/model/ProductHistory.java
+++ src/main/java/com/productShop/inventarization/model/ProductHistory.java
@@ -7,6 +7,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import java.time.LocalDate;
@@ -30,7 +31,7 @@ public class ProductHistory {
     @Column(name = "id", nullable = false)
     Long id;
 
-    @OneToOne(cascade = CascadeType.ALL)
+    @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "product_id")
     Product product;
 
