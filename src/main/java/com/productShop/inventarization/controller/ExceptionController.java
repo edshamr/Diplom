8aa4846d8147ef63f1c3039bbf00b966ca6faac4
eduard-shamrai <eduard.shamrai@gmail.com>
2024-05-15 02:17:42 +0300

    third commit

--- src/main/java/com/productShop/inventarization/controller/ExceptionController.java
+++ src/main/java/com/productShop/inventarization/controller/ExceptionController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class ExceptionController {
     @ExceptionHandler(RuntimeException.class)
-    public String allExceptions(Model model, RuntimeException ex) {
+    public String handleExceptions(Model model, RuntimeException ex) {
         model.addAttribute("errorMessage", ex.getMessage());
         return "error";
     }
